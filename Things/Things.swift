6e344124a42a864094d63c509b527a8ce818b139
Sergey Pugach <sp@voir.me>
2021-03-26 20:17:37 +0300

    up

--- Things/Things.swift
+++ Things/Things.swift
@@ -1,6 +1,6 @@
 //
 //  AppView.swift
-//  VoirCam
+//  Things
 //
 //  Created by Sergey Pugach on 12/9/20.
 //
@@ -9,26 +9,54 @@ import SwiftUI
 import ComposableArchitectureApp
 import Firebase
 import Root
+import Welcome
+import Authentication
 
 @main
-struct VoirCam: App {
+struct Things: App {
+    
+    private let authentication: Authenticatable
+    @State var isLogged = false
     
     init() {
         
         //Or better configure in another place/module
         FirebaseConfiguration.shared.setLoggerLevel(.debug)
         FirebaseApp.configure()
+        
+        authentication = Authentication.live //Authentication()
     }
     
+    @ViewBuilder
     var body: some Scene {
         WindowGroup {
-            Root.RootView(
-                store: Store<Root.State, Root.Action>(
-                    initialState: Root.State(profile: .initial),
-                    reducer: Root.reducer,
-                    environment: Root.Environment()
+            if isLogged {
+                root
+            } else {
+                welcome
+            }
+        }
+    }
+    
+    private var root: some View {
+        Root.RootView(
+            store: Store<Root.State, Root.Action>(
+                initialState: Root.State(profile: .initial),
+                reducer: Root.reducer,
+                environment: Root.Environment()
+            )
+        )
+    }
+    
+    private var welcome: some View {
+        Welcome.WelcomeView(
+            store: ComposableArchitecture.Store<Welcome.State, Welcome.Action>(
+                initialState: .initial,
+                reducer: Welcome.reducer,
+                environment: Welcome.Environment(
+                    authentication: authentication
                 )
             )
-        }
+        )
     }
 }
